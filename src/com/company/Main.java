e980f2582e64682ba9dee2ce2b6cc5a896443dce
guler <gule0153@stud.kea.dk>
2021-12-06 21:26:35 +0100

    deleted unnecessary comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,11 @@
 package com.company;
 
-import com.company.data.Database;
-import com.company.data.FileHandler;
 import com.company.domain.Controller;
 
 
 public class Main {
 
-
     public static void main(String[] Args){
-        /*Database db = new Database();
-        new FileHandler().readFile();
-        System.out.println(db.findMember("Marc"));*/
         new Controller().start();
-
     }
-
 }
